3da4b73fccf0f011ad1d8f92dc014005388b0fe6
Walter Popovich <sone969@hotmail.com>
2017-12-18 18:14:01 -0300

    Improved handling of pads and balls position. This should help to ease the way for collisions
    Signed-off-by: Walter Popovich <sone969@hotmail.com>

--- core/src/com/mygdx/pong/PongGame.java
+++ core/src/com/mygdx/pong/PongGame.java
@@ -2,20 +2,25 @@ package com.mygdx.pong;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.mygdx.pong.commons.PongGameRules;
+import com.mygdx.pong.commons.balls.Ball;
 import com.mygdx.pong.commons.input.PlayerOneController;
 import com.mygdx.pong.commons.input.interfaces.PlayerTwoController;
 import com.mygdx.pong.commons.playable.player.Player;
+
+//TODO: In the future, remove global rules, instead use some sort of map class to have different levels with different rules.
 
 public class PongGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	ShapeRenderer shapeRenderer;
 	Player player;
 	Player player2;
+	Ball gameBall;
 
 	Texture gameBackground;
 
@@ -23,14 +28,15 @@ public class PongGame extends ApplicationAdapter {
 	public void create () {
 		this.resize(PongGameRules.SCREEN_WIDTH, PongGameRules.SCREEN_HEIGHT);
 		batch = new SpriteBatch();
-//		img = new Texture("badlogic.jpg");
-		gameBackground = new Texture("MainBackground.jpg");
+		gameBackground = new Texture(new FileHandle("MainBackground.jpg"), true);
 		shapeRenderer = new ShapeRenderer();
 
-		//TODO: Improve playerController to avoid instance, maybe some sort of dependency injection?
+		//TODO: Improve playerController to avoid instantiating, maybe some sort of dependency injection?
 		//TODO: Also improve creation of the pad.
 		player = new Player(new PlayerOneController(), shapeRenderer, PongGameRules.SCREEN_WIDTH / 2, PongGameRules.FIELD_VERTICAL_BOUNDS);
 		player2 = new Player(new PlayerTwoController(), shapeRenderer, PongGameRules.SCREEN_WIDTH / 2, PongGameRules.SCREEN_HEIGHT - PongGameRules.FIELD_VERTICAL_BOUNDS);
+
+		gameBall = new Ball(shapeRenderer);
 
 	}
 
@@ -39,22 +45,24 @@ public class PongGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(gameBackground, 0,0);
+		batch.draw(gameBackground, 0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.end();
 
 		player.getController().movePlayer(player.getPlayerPad());
 		player2.getController().movePlayer(player2.getPlayerPad());
+		gameBall.move();
 
 		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
 		player.getPlayerPad().draw();
 		player2.getPlayerPad().draw();
+		gameBall.draw();
 		shapeRenderer.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-//		img.dispose();
 		shapeRenderer.dispose();
+		gameBackground.dispose();
 	}
 }
